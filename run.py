9f84bd44deaf8c3e54523ba62110703503025072
gnisco <gnisco08@gmail.com>
2020-02-21 02:42:46 +0000

    bug fix navbar and tweeks

--- run.py
+++ run.py
@@ -24,6 +24,10 @@ def bloghome2():
 @app.route('/blog-post')
 def blogpost():
     return render_template("blog-post.html")
+
+@app.route('/book-appointments')
+def bookappointments():
+    return render_template("book-appointments.html")
     
 @app.route('/contact')
 def contact():
@@ -76,6 +80,6 @@ def sidebar():
     return render_template("sidebar.html")
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+            port=int(os.environ.get('PORT', '5000')),
             debug=True)
