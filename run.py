64afe9655613cd3eac103525b8680f819dbb2443
gnisco <gnisco08@gmail.com>
2020-04-02 16:12:51 +0000

    style fixes and booking form fix for calendar

--- run.py
+++ run.py
@@ -143,3 +143,13 @@ if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=int(os.environ.get('PORT', '5000')),
             debug=True)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('admin_login.html', error=error)
